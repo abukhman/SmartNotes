53ab7030fdd337c898cbbdb3314da84bc49c3e53
Mike <s02200482@gse.cs.msu.ru>
2023-10-11 19:23:11 +0300

    Added audio upload function

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "very long and powerful secretkey"
+app.config['UPLOAD_PATH'] = 'static/uploads'
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  #100 МБ
+
+if not os.path.exists(app.config['UPLOAD_PATH']):
+    os.mkdir(app.config['UPLOAD_PATH'])
+
 menu = [{"name": "Home", "url": "/"},
         {"name": "History", "url": "history"},
         {"name": "Convert", "url": "convert"}]
+
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 @app.route("/")
 def home():
@@ -12,10 +27,17 @@ def home():
 
 @app.route("/history")
 def history():
-    return render_template('history.html', menu=menu)
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('history.html', menu=menu, files=files)
 
-@app.route("/convert")
-def generate():
+@app.route("/convert", methods=['GET', 'POST'])
+def convert():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        if filename != '':
+            file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            return render_template('convert.html', menu=menu, filename=filename)
     return render_template('convert.html', menu=menu)
 
 if __name__ == "__main__":
