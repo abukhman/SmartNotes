5ccabc1f5214326a03197d6eb1ed8bba085fcac9
noise <noise.martz@gmail.com>
2023-11-23 15:19:31 +0300

    certs + async

--- app.py
+++ app.py
@@ -1,65 +1,93 @@
 import os
 import tempfile
 
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "very long and powerful secretkey"
+app.config["UPLOAD_PATH"] = "static/uploads"
+app.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024  # 100 МБ
+app.config["CERT_PATH"] = "certs.pem"
 
 from convertor.salute_convertor import SaluteConvertor
 
+if not os.path.exists(app.config["UPLOAD_PATH"]):
+    os.mkdir(app.config["UPLOAD_PATH"])
 
+menu = [
+    {"name": "Home", "url": "/"},
+    {"name": "History", "url": "history"},
+    {"name": "Convert", "url": "convert"},
+]
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "very long and powerful secretkey"
-app.config['UPLOAD_PATH'] = 'static/uploads'
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  #100 МБ
-
-if not os.path.exists(app.config['UPLOAD_PATH']):
-    os.mkdir(app.config['UPLOAD_PATH'])
-
-menu = [{"name": "Home", "url": "/"},
-        {"name": "History", "url": "history"},
-        {"name": "Convert", "url": "convert"}]
 
 @app.route("/")
 def home():
-    return render_template('home.html', menu=menu)
+    return render_template("home.html", menu=menu)
+
 
 @app.route("/history")
 def history():
-    files = os.listdir(app.config['UPLOAD_PATH'])
-    return render_template('history.html', menu=menu, files=files)
+    files = os.listdir(app.config["UPLOAD_PATH"])
+    return render_template("history.html", menu=menu, files=files)
 
-@app.route('/notes')
+
+@app.route("/notes")
 def notes():
     args = request.args
-    id = args.get('id')
-    return render_template('notes.html', menu=menu, id=id)
+    id = args.get("id")
+    return render_template("notes.html", menu=menu, id=id)
 
-@app.route("/convert", methods=['GET', 'POST'])
+
+@app.route("/convert", methods=["GET", "POST"])
 def convert():
-    if request.method == 'POST':
-        auth = request.form['auth']
-        file = request.files['file']
+    if request.method == "POST":
+        kind = "sync"
+        auth = request.form["auth"]
+        file = request.files["file"]
         tmp = tempfile.TemporaryFile()
-
-        # tmp_path = os.path.join(tempfile.gettempdir(), tmp.name)
-        # print(tmp_path)
 
         file.save(tmp)
         # начинаем с начала, потому что после save указатель в конце
         tmp.seek(0)
 
         conv = SaluteConvertor()
+        conv.get_token(auth)
+
+        if kind == "sync":
+            result = conv.sync_recognition(file.mimetype, tmp)
+
+            text = ""
+            if result != None and len(result) > 0:
+                text = result[0]
+                for i in range(1, len(result)):
+                    text += result[i]
+            return {"text": text, "redirect_url": "/notes"}
+        else:
+            task_id = conv.async_recognition(file.mimetype, tmp)
+
+            return {"task_id": task_id}
+
+    return render_template("convert.html", menu=menu)
+
+
+@app.route("/check_task", methods=["GET", "POST"])
+def check_task():
+    if request.method == "POST":
+        auth = request.form["auth"]
+        task_id = request.form["task_id"]
+
+        conv = SaluteConvertor()
         conv.login(auth)
-        result = conv.sync_recognition(file.mimetype, tmp)
-        
-        text = ""
+
+        result = conv.check_and_download(task_id)
         if result != None and len(result) > 0:
             text = result[0]
             for i in range(1, len(result)):
                 text += result[i]
-        return {'text': text, 'redirect_url': '/notes'}
+            return {"text": text, "redirect_url": "/notes"}
+        return {}
 
-    return render_template('convert.html', menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- convertor/salute_convertor.py
+++ convertor/salute_convertor.py
@@ -2,6 +2,7 @@ import datetime as dt
 import json, uuid, requests, enum
 from werkzeug.datastructures import FileStorage
 from convertor.convertor import Convertor
+from app import app
 
 url_auth = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 url_upload = "https://smartspeech.sber.ru/rest/v1/data:upload"
@@ -9,6 +10,7 @@ url_async = "https://smartspeech.sber.ru/rest/v1/speech:async_recognize"
 url_sync = "https://smartspeech.sber.ru/rest/v1/speech:recognize"
 url_get = "https://smartspeech.sber.ru/rest/v1/task:get"
 url_download = "https://smartspeech.sber.ru/rest/v1/data:download"
+
 
 class StatusType(enum.StrEnum):
     Undefined = "UNDEFINED"
@@ -18,111 +20,195 @@ class StatusType(enum.StrEnum):
     Done = "DONE"
     Error = "ERROR"
 
+
 class SaluteConvertor(Convertor):
     def __init__(self):
         self.token = ""
         self.expires = dt.datetime.now()
 
-    def login(self, secret, scope="SALUTE_SPEECH_PERS"):
+    def get_token(self, secret, scope="SALUTE_SPEECH_PERS") -> str:
         request_uuid = uuid.uuid4()
-        headers = {"Authorization": "Basic %s" %secret, "RqUID": str(request_uuid)}
+        headers = {"Authorization": "Basic %s" % secret, "RqUID": str(request_uuid)}
 
         response: requests.Response
         try:
-            response = requests.post(url_auth, headers=headers, data={"scope": scope})
+            response = requests.post(
+                url_auth,
+                headers=headers,
+                data={"scope": scope},
+                verify=app.config["CERT_PATH"],
+            )
         except Exception as e:
-            print(e)
-            return
+            app.logger.error("unable to get auth token: %s", e)
+            return ""
 
         if not response.ok:
-            print(response.json())
-            return
+            app.logger.warning("got bad response: %s", response.json())
+            return ""
 
         data = response.json()
 
         self.token = data.get("access_token", "")
         self.expires = dt.datetime.fromtimestamp(data.get("expires_at", 0) / 1000.0)
 
+        return self.token
+
     def sync_recognition(self, mimetype, stream):
         if self.token == "" or dt.datetime.now() > self.expires:
-            print("login token was expired")
+            app.logger.info(
+                "token is expired or do not exist: token: %s, expired_at: %s",
+                self.token,
+                self.expires,
+            )
             return
 
-        headers={"Authorization": "Bearer %s" %self.token,
-                 "Content-Type": mimetype}
+        headers = {"Authorization": "Bearer %s" % self.token, "Content-Type": mimetype}
 
-        data = stream.read()
+        file = stream.read()
         stream.close()
 
         response: requests.Response
         try:
-            response = requests.post(url_sync, headers=headers, data=data)
+            response = requests.post(
+                url_sync,
+                headers=headers,
+                data=file,
+                verify=app.config["CERT_PATH"],
+            )
         except Exception as e:
-            print(e)
+            app.logger.error("unable to sync recognize: %s", e)
             return
 
         if not response.ok:
-            print(response.json())
+            app.logger.warning("got bad response: %s", response.json())
             return
 
         data = response.json()
         if data.get("status", 0) != 200:
-            print(data)
+            app.logger.warning("got bad response: %s", data)
             return
 
         return data.get("result", [])
 
-
-    def load_file(self, file) -> str | None:
-        if len(self.token) == 0 or dt.datetime.now() > dt.datetime.fromtimestamp(self.expires):
-            # to-do сказать пользователю об этом
+    def async_recognition(self, mimetype, stream):
+        request_file_id = self.load_file()
+        if len(request_file_id) == 0:
+            app.logger.error("unable to get request_file_id")
             return
 
-        headers = {"Authorization": "Bearer "+self.token}
-        response = requests.post(url_upload, headers=headers, data=file)
+        self.create_task()
 
-        if response.status_code != 200:
-            print(response.json())
-            return
+    def load_file(self, mimetype, stream) -> str:
+        if self.token == "" or dt.datetime.now() > self.expires:
+            app.logger.info(
+                "token is expired or do not exist: token: %s, expired_at: %s",
+                self.token,
+                self.expires,
+            )
+            return ""
 
-        return response.json()["result"]["request_file_id"]
+        headers = {"Authorization": "Bearer %s" % self.token, "Content-Type": mimetype}
+
+        file = stream.read()
+        stream.close()
+
+        response: requests.Response
+        try:
+            response = requests.post(
+                url_upload,
+                headers=headers,
+                data=file,
+                verify=app.config["CERT_PATH"],
+            )
+        except Exception as e:
+            app.logger.error("unable to upload file: %s", e)
+            return ""
+
+        if not response.ok:
+            app.logger.warning("got bad response: %s", response.json())
+            return ""
+
+        data = response.json()
+        if data.get("status", 0) != 200:
+            app.logger.warning("got bad response: %s", data)
+            return ""
+
+        result = data.get("result", {})
+        return result.get("request_file_id", "")
 
     # create task for uploaded file
-    def create_task(self, file_id) -> str | None:
-        headers = {"Authorization": "Bearer %s" %self.token}
+    def create_task(self, file_id) -> str:
+        headers = {"Authorization": "Bearer %s" % self.token}
         params = {
             "request_file_id": file_id,
             "options": {
                 "audio_encoding": "OPUS",
                 "language": "ru-RU",
-            }
+            },
         }
 
-        response = requests.post(url_async, headers=headers, data=json.dumps(params, indent = 4))
-        if response.status_code != 200:
-            print(response.json())
-            return
+        response: requests.Response
+        try:
+            response = requests.post(
+                url_async,
+                headers=headers,
+                data=json.dumps(params, indent=4),
+                verify=app.config["CERT_PATH"],
+            )
+        except Exception as e:
+            app.logger.error("unable to upload file: %s", e)
+            return ""
 
-        return response.json()["result"]["id"]
+        if not response.ok:
+            app.logger.warning("got bad response: %s", response.json())
+            return ""
 
+        data = response.json()
+        if data.get("status", 0) != 200:
+            app.logger.warning("got bad response: %s", data)
+            return ""
+
+        result = data.get("result", {})
+        return result.get("id", "")
 
     def check_task(self, task_id):
-        headers = {"Authorization": "Bearer %s" %self.token}
+        headers = {"Authorization": "Bearer %s" % self.token}
 
-        response = requests.get(url_get, headers=headers, data={"id": task_id})
+        response = requests.get(
+            url_get,
+            headers=headers,
+            data={"id": task_id},
+            verify=app.config["CERT_PATH"],
+        )
         if response.status_code != 200:
             print(response.json())
             return
-        return response.json()["result"]["status"], response.json()["result"]["request_file_id"]
+        return (
+            response.json()["result"]["status"],
+            response.json()["result"]["request_file_id"],
+        )
 
     def download_file(self, response_id):
-        headers = {"Authorization": "Bearer %s" %self.token}
+        headers = {"Authorization": "Bearer %s" % self.token}
 
-        response = requests.get(url_download, headers=headers, data={"response_file_id": response_id})
+        response = requests.get(
+            url_download,
+            headers=headers,
+            data={"response_file_id": response_id},
+            verify=app.config["CERT_PATH"],
+        )
         if response.status_code != 200:
             print(response.json())
             return
         return response
 
+    def check_and_download(self, task_id) -> list | None:
+        result = self.check_task(task_id)
+        if result[0] == "DONE":
+            return self.download_file(result[1])
+
+        return None
+
     def convert_data(self, data):
+        pass
         pass
